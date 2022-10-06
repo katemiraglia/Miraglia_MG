8d93f7a18d9e0fec192cdd4553dbaf4361ced639
katemiraglia <102632409+katemiraglia@users.noreply.github.com>
2022-10-06 01:24:20 -0400

    Update scripts.js

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,8 +3,14 @@ import {
 } from "gsap";
 
 
-gsap.from("#hero", {
-    duration: 1.5,
+gsap.from("#first-line", {
+    duration: 1,
     x: -200,
     alpha: 0
+});
+gsap.from("#second-line", {
+    duration: 1,
+    x: -100,
+    alpha: 0,
+    delay: 0.5
 });
